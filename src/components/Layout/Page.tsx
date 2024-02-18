8bb403e4a2ceab77799f5aec0f505e5da96ac255
heyli <lcxfs1991@gmail.com>
2024-02-18 15:47:33 +0800

    feat: improve

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -199,7 +199,7 @@ export function Page({children, toc, routeTree, meta, section}: PageProps) {
         <div
           className="wwads-cn wwads-vertical wwads-sticky"
           data-id="306"
-          style={{position: 'fixed', bottom: 0, right: 10, maxWidth: '240px'}}
+          style={{position: 'fixed', bottom: 0, right: 10, maxWidth: '200px'}}
         />
       </div>
     </>
